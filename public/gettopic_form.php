b8e43adf5790f905ad62d4348fe2a24af9e11be9
nshiban678 <nshiban678@gmail.com>
2016-10-14 05:09:22 -0400

    Add files via upload

---
+++ public/gettopic_form.php
@@ -0,0 +1,17 @@
+<form action="gettopic.php" method="post">
+
+    <fieldset>
+        <div class="form-group">
+            <input autofocus class="form-control" name="symbol" placeholder="Stock" type="text"/>
+        </div>
+        <div class="form-group">
+            <input autofocus class="form-control" name="num_buy" placeholder="Shares" type="text"/>
+        </div>
+        <div class="form-group">
+        <button type="submit" class="btn btn-default">Buy</button>
+        </div>
+        <div>
+            <a href = "index.php">Go back to index</a>
+        </div>
+    </fieldset>
+</form>
